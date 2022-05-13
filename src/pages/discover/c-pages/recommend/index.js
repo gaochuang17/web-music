caca2ea6714e4d37f4930a7f1127dca8bf93aad0
gaochuangchuang <m17864291492@163.com>
2022-05-13 13:55:59 +0800

    解决useSelector性能问题

--- src/pages/discover/c-pages/recommend/index.js
+++ src/pages/discover/c-pages/recommend/index.js
@@ -1,13 +1,16 @@
 import React, { memo, useEffect } from "react";
-import { connect, useDispatch, useSelector } from "react-redux";
+import { connect, useDispatch, useSelector, shallowEqual } from "react-redux";
 
 import { getTopBannerAction } from "./store/actionCreators";
 
 const Recommend = memo((props) => {
   //组件和redux关联：获取数据和进行操作
-  const { topBanners } = useSelector((state) => ({
-    topBanners: state.recommend.topBanners,
-  }));
+  const { topBanners } = useSelector(
+    (state) => ({
+      topBanners: state.recommend.topBanners,
+    }),
+    shallowEqual
+  );
 
   const dispatch = useDispatch();
 
