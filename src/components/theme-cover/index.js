13a2cf5223e7f5c54ef0275dae75c1b6f2e146bf
gaochuangchuang <m17864291492@163.com>
2022-05-16 10:57:52 +0800

    获取小图片和推荐数目的工具函数的封装

--- src/components/theme-cover/index.js
+++ src/components/theme-cover/index.js
@@ -1,5 +1,7 @@
 import React, { memo } from "react";
 
+import { getSizeImage } from "@/utils/format-utils";
+
 import { ThemeCoverWrapper } from "./style";
 
 const ThemeCover = memo((props) => {
@@ -7,7 +9,7 @@ const ThemeCover = memo((props) => {
   return (
     <ThemeCoverWrapper>
       <div className="cover-out">
-        <img className="image" src={info.picUrl}></img>
+        <img className="image" src={getSizeImage(info.picUrl, 100)}></img>
         <div className="cover-name">{info.name}</div>
         <div className="cover-source">by {info.copywriter}</div>
       </div>
