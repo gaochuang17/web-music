787e1ba378f2218b2280ce2d586cadbc9a2784f7
gaochuangchuang <m17864291492@163.com>
2022-05-23 22:02:19 +0800

    路由懒加载

--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import React from "react";
 import { Navigate } from "react-router-dom";
 
-import Discover from "@/pages/discover";
-import Friend from "@/pages/friend";
-import Mine from "@/pages/mine";
-import Recommend from "../pages/discover/c-pages/recommend";
-import Ranking from "../pages/discover/c-pages/ranking";
-import Songs from "../pages/discover/c-pages/songs";
-import Djradio from "../pages/discover/c-pages/djradio";
-import Artist from "../pages/discover/c-pages/artist";
-import Album from "../pages/discover/c-pages/album";
-import Player from "../pages/player";
+const Discover = React.lazy(() => import("@/pages/discover"));
+const Friend = React.lazy(() => import("@/pages/friend"));
+const Mine = React.lazy(() => import("@/pages/mine"));
+const Recommend = React.lazy(() =>
+  import("../pages/discover/c-pages/recommend")
+);
+const Ranking = React.lazy(() => import("../pages/discover/c-pages/ranking"));
+const Songs = React.lazy(() => import("../pages/discover/c-pages/songs"));
+const Djradio = React.lazy(() => import("../pages/discover/c-pages/djradio"));
+const Artist = React.lazy(() => import("../pages/discover/c-pages/artist"));
+const Album = React.lazy(() => import("../pages/discover/c-pages/album"));
+const Player = React.lazy(() => import("../pages/player"));
+
+// import Discover from "@/pages/discover";
+// import Friend from "@/pages/friend";
+// import Mine from "@/pages/mine";
+// import Recommend from "../pages/discover/c-pages/recommend";
+// import Ranking from "../pages/discover/c-pages/ranking";
+// import Songs from "../pages/discover/c-pages/songs";
+// import Djradio from "../pages/discover/c-pages/djradio";
+// import Artist from "../pages/discover/c-pages/artist";
+// import Album from "../pages/discover/c-pages/album";
+// import Player from "../pages/player";
 
 const routes = [
   {
